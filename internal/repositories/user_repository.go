af967f2d7beb958329bb728470337408658aac17
Hettank <hettank34@gmail.com>
2026-06-30 01:01:27 +0530

    Error management and user creation through repository and service flow structure

---
+++ internal/repositories/user_repository.go
@@ -0,0 +1,80 @@
+package repositories
+
+import (
+	"context"
+	"errors"
+
+	"github.com/Hettank/habit-tracker/internal/models"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type UserRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewUserRepository(db *pgxpool.Pool) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (r *UserRepository) GetByEmail(ctx context.Context, email string) (*models.User, error) {
+	query := `
+		SELECT
+			id,
+			email,
+			password_hash,
+			created_at
+		FROM users
+		WHERE email = $1
+	`
+
+	var user models.User
+
+	err := r.db.QueryRow(
+		ctx,
+		query,
+		email,
+	).Scan(
+		&user.ID,
+		&user.Email,
+		&user.PasswordHash,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *UserRepository) Create(ctx context.Context, user *models.User) error {
+	query := `
+		INSERT INTO users (
+			email,
+			password_hash
+		)
+		VALUES (
+			$1,
+			$2
+		)
+		RETURNING id, created_at
+	`
+
+	err := r.db.QueryRow(
+		ctx,
+		query,
+		user.Email,
+		user.PasswordHash,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+
+	return err
+}
---
+++ internal/services/auth_service.go
@@ -0,0 +1,60 @@
+package services
+
+import (
+	"context"
+
+	"github.com/Hettank/habit-tracker/internal/dto"
+	apperrors "github.com/Hettank/habit-tracker/internal/errors"
+	"github.com/Hettank/habit-tracker/internal/models"
+	"github.com/Hettank/habit-tracker/internal/repositories"
+	"github.com/Hettank/habit-tracker/internal/utils"
+)
+
+// whenever the service needs user data, it asks the repository.
+type AuthService struct {
+	userRepo *repositories.UserRepository
+}
+
+func NewAuthService(userRepo *repositories.UserRepository) *AuthService {
+	return &AuthService{
+		userRepo: userRepo,
+	}
+}
+
+func (s *AuthService) Register(ctx context.Context, req dto.RegisterRequest) (*models.User, error) {
+	// Step 1: Check if email already exists.
+	existingUser, err := s.userRepo.GetByEmail(ctx, req.Email)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if existingUser != nil {
+		return nil, apperrors.ErrUserAlreadyExists
+	}
+
+	// Step 2: Hash password.
+	hashedPassword, err := utils.HashPassword(
+		req.Password,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Step 3: Create domain model
+	user := &models.User{
+		Email:        req.Email,
+		PasswordHash: hashedPassword,
+	}
+
+	// Step 4: save user
+	err = s.userRepo.Create(ctx, user)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// return the created user.
+	return user, nil
+}
